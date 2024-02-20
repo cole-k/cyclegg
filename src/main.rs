c4b23c26a3884eb4db36b3c3b54c70a9b8ed3e37
Cole K <cole-k@users.noreply.github.com>
2024-02-19 21:29:00 -0800

    Change output format to track number of lemmas
    Rudimentary right now, but I made a breaking change to the result file
    format

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,9 @@ fn main() -> Result<()> {
   } else {
     None
   };
+  result_file.iter_mut().for_each(|file| {
+    file.write(format!("name,result,time,num_lemmas,num_lemmas_attempted,num_lemmas_proven,result_cyclic,time_cyclic,num_lemmas_cyclic,num_attempted_lemmas_cyclic,num_proven_lemmas_cyclic").as_bytes()).expect("failed to write header");
+  });
   let mut num_goals_attempted = 0;
   let mut num_differing_goals = 0;
   let mut cyclic_num_valid = 0;
@@ -59,16 +62,16 @@ fn main() -> Result<()> {
       goal
     );
 
-    let (result, duration) = if ARGS.do_uncyclic() {
+    let (result, duration, num_lemmas, num_attempted_lemmas, num_proven_lemmas) = if ARGS.do_uncyclic() {
       prove_goal(goal.clone(), raw_goal.prop.clone(), raw_goal.premise.clone(), global_search_state, false)?
     } else {
-      (Outcome::Unknown, Duration::from_secs(0))
+      (Outcome::Unknown, Duration::from_secs(0), 0, 0, 0)
     };
-    let (result_cyclic, duration_cyclic) = if ARGS.do_cyclic() {
+    let (result_cyclic, duration_cyclic, num_lemmas_cyclic, num_attempted_lemmas_cyclic, num_proven_lemmas_cyclic) = if ARGS.do_cyclic() {
       goal.name = format!("{}_cyclic", goal.name);
       prove_goal(goal.clone(), raw_goal.prop.clone(), raw_goal.premise.clone(), global_search_state, true)?
     } else {
-      (Outcome::Unknown, Duration::from_secs(0))
+      (Outcome::Unknown, Duration::from_secs(0), 0, 0, 0)
     };
 
     if CONFIG.verbose {
@@ -78,6 +81,13 @@ fn main() -> Result<()> {
         raw_goal.name.blue(),
         goal
       );
+    }
+
+    if ARGS.do_uncyclic() {
+      println!("(uncyclic) Theorized {} lemmas, fully attempted {} lemmas, proved {} lemmas", num_lemmas, num_attempted_lemmas, num_proven_lemmas);
+    }
+    if ARGS.do_cyclic() {
+      println!("(cyclic) Theorized {} lemmas, fully attempted {} lemmas, proved {} lemmas", num_lemmas_cyclic, num_attempted_lemmas_cyclic, num_proven_lemmas_cyclic);
     }
 
     if ARGS.do_cyclic() && ARGS.do_uncyclic() {
@@ -116,13 +126,20 @@ fn main() -> Result<()> {
     }
     if let Some(ref mut file) = result_file {
       let line = format!(
-        "{},{:?},{:?},{},{}\n",
+        "{},{:?},{},{},{},{},{:?},{},{},{},{}\n",
         raw_goal.name,
-        result_cyclic,
         result,
         // Convert to ms
-        1000. * duration_cyclic.as_secs_f32(),
         1000. * duration.as_secs_f32(),
+        num_lemmas,
+        num_attempted_lemmas,
+        num_proven_lemmas,
+        result_cyclic,
+        // Convert to ms
+        1000. * duration_cyclic.as_secs_f32(),
+        num_lemmas_cyclic,
+        num_attempted_lemmas_cyclic,
+        num_proven_lemmas_cyclic,
       );
       file.write_all(line.as_bytes())?;
     }
@@ -142,7 +159,7 @@ fn main() -> Result<()> {
 
 /// Prove a goal using either cyclic or uncyclic mode;
 /// record the duration and emit the proof.
-fn prove_goal<'a>(goal: Goal<'a>, goal_prop: Prop, goal_premise: Option<Equation>, global_search_state: GlobalSearchState<'a>, cyclic: bool) -> Result<(Outcome, Duration)> {
+fn prove_goal<'a>(goal: Goal<'a>, goal_prop: Prop, goal_premise: Option<Equation>, global_search_state: GlobalSearchState<'a>, cyclic: bool) -> Result<(Outcome, Duration, usize, usize, usize)> {
   CONFIG.set_cyclic(cyclic);
   let start_time = Instant::now();
   if CONFIG.verbose {
@@ -152,6 +169,21 @@ fn prove_goal<'a>(goal: Goal<'a>, goal_prop: Prop, goal_premise: Option<Equation
     }
   }
   let (result, mut proof_state) = goal::prove_top(goal_prop, goal_premise, global_search_state);
+  let mut num_lemmas = 0;
+  let mut num_proven_lemmas = 0;
+  let mut num_lemmas_not_attempted = 0;
+  for (lemma_number, lemma_proof_state) in proof_state.lemma_proofs.iter() {
+    if lemma_number == &0 {
+      continue;
+    }
+    num_lemmas += 1;
+    if lemma_proof_state.outcome == Some(Outcome::Valid) {
+      num_proven_lemmas += 1;
+    }
+    if lemma_proof_state.outcome == None {
+      num_lemmas_not_attempted += 1;
+    }
+  }
   let duration = start_time.elapsed();
   if CONFIG.emit_proofs {
     if let Outcome::Valid = result {
@@ -187,5 +219,5 @@ fn prove_goal<'a>(goal: Goal<'a>, goal_prop: Prop, goal_premise: Option<Equation
   //   }
   // }
 
-  Ok((result, duration))
+  Ok((result, duration, num_lemmas, num_lemmas - num_lemmas_not_attempted, num_proven_lemmas))
 }
